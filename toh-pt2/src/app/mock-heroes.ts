ccc90d8438b3b31f45f14d6fc3401ddcaabd1fbc
VelvetRose252 <60297139+VelvetRose252@users.noreply.github.com>
2022-09-30 19:20:42 -0400

    Add files via upload

---
+++ toh-pt2/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Mihai Boicu' },
+  { id: 13, name: 'Michael Lyons' },
+  { id: 14, name: 'Valriy Pavlenko' },
+  { id: 15, name: 'John McLaughlin' },
+  { id: 16, name: 'Pyouyan Ahmadi' },
+  { id: 17, name: 'Jack Laveson' },
+  { id: 18, name: 'Can Nguyen' },
+  { id: 19, name: 'Margaret Leary' },
+  { id: 20, name: 'Scott Lewis' }
+];
